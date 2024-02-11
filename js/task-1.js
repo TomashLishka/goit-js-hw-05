7d6680e4c2e1688b177965b3d1af9c3e1f76ada3
Tomash Lishka <tl9242972@gmail.com>
2024-02-11 15:28:45 +0200

    hw-5

--- js/task-1.js
+++ js/task-1.js
@@ -1,34 +1,45 @@
-const books = [
-    {
-      title: "The Last Kingdom",
-      author: "Bernard Cornwell",
-      rating: 8.38,
-    },
-    {
-      title: "Beside Still Waters",
-      author: "Robert Sheckley",
-      rating: 8.51,
-    },
-    {
-      title: "The Dream of a Ridiculous Man",
-      author: "Fyodor Dostoevsky",
-      rating: 7.75,
-    },
-    {
-      title: "Redder Than Blood",
-      author: "Tanith Lee",
-      rating: 7.94,
-    },
-    {
-      title: "Enemy of God",
-      author: "Bernard Cornwell",
-      rating: 8.67,
-    },
-  ];
-  
-  const sortedByAuthorName = books.map(book => book.toSorted((a, b) => {
-    a.localeCompare(b);
-  }) 
-    );
-  console.log(sortedByAuthorName);
-  
+function getUserNames(users) {
+  const getName = users.flatMap(user => user.name);
+  return getName;
+}
+
+console.log(
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
